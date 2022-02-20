bd622ec3500fed4e990a8cf2c06b329c32466720
JuanJoseEscobar <cheche.juan@hotmail.com>
2022-02-20 14:22:57 -0500

    realizacion del cdigo 0.1

--- Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
+++ Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
@@ -19,7 +19,51 @@ namespace PrintPrimes
 
     private static void PrintPrimes(int number)
     {
-      throw new NotImplementedException();
+      if(number == 1)
+      {
+        Console.Write("Error: 1 Is not Prime\n");
+        return;
+      }else if(number <= 0)
+      {
+        Console.Write("Error: Invalid Number\n");
+        return;
+      }
+
+      for (int i = 1; i <= number; i++)
+      {
+        if(IsPrime(i))
+        {
+          Console.Write(i.ToString()+", ");
+        }
+      }
+      Console.Write("\n");
+
+      //throw new NotImplementedException();
     }
+
+    private static bool IsPrime(int number){
+      int contDivider = 0;
+
+      for (int i = 1; i <= number; i++)
+      {
+        if (number % i == 0)
+        {
+          contDivider += 1;
+        }
+        if (contDivider>2)
+        {
+          return false;
+        }
+      }
+
+      if (contDivider == 2)
+      {
+        return true;
+      }
+
+      return false;
+    }
+    
   }
 }
+

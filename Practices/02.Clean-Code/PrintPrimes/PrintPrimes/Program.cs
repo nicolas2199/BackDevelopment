780c2e7451ce5cc0c94e969fc699a81cbe2228d1
heidy1523 <heidy1523diaz@gmail.com>
2022-02-22 20:17:55 -0500

    realice el codigo

--- Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
+++ Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
@@ -2,24 +2,49 @@
 
 namespace PrintPrimes
 {
-  internal class Program
-  {
-    static void Main(string[] args)
+    internal class Program
     {
-      PrintPrimes(13);
-      // 2,3,5,7,11,13
-      PrintPrimes(10);
-      // 2,3,5,7
-      PrintPrimes(0);
-      // Error: Invalid Number
-      PrintPrimes(1);
-      // Error: 1 Is not Prime
+        static void Main(string[] args)
+        {
+            PrintPrimes(13);
+            // 2,3,5,7,11,13
+            PrintPrimes(10);
+            // 2,3,5,7
+            PrintPrimes(0);
+            // Error: Invalid Number
+            PrintPrimes(1);
+            // Error: 1 Is not Prime
 
-    }
+        }
 
-    private static void PrintPrimes(int number)
-    {
-      throw new NotImplementedException();
+        private static void PrintPrimes(int number)
+        {
+            int contador = 0;
+
+
+            for (int i = 1; i < (number + 1); i++)
+            {
+                if (number % i == 0)
+                {
+                    contador++;
+                }
+            }
+            if (number == 0)
+            {
+                Console.WriteLine("Error: Numero invalido");
+            }
+            if (contador != 2)
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("1 no es primo");
+            }
+
+        }
     }
-  }
 }
+
+
+

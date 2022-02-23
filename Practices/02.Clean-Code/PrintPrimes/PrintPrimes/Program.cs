00156191a5f3757852a7aaeb3cfafb2fb82ffe43
XMunteR <89564026+XMunteR@users.noreply.github.com>
2022-02-22 19:52:16 -0500

    primes

--- Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
+++ Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
@@ -6,36 +6,46 @@ namespace PrintPrimes
   {
     static void Main(string[] args)
     {
+     
+      Console.WriteLine ("Primos hasta 13");
       PrintPrimes(13);
       // 2,3,5,7,11,13
+     
+      Console.WriteLine ("Primos hasta 10");
       PrintPrimes(10);
       // 2,3,5,7
-      // Error: Invalid Number
+      
+      Console.WriteLine ("Primos hasta 1");
       PrintPrimes(1);
       // Error: 1 Is not Prime
+      
+      Console.WriteLine ("Primos hasta 0");
+      PrintPrimes(0);
+      
+      // Error: Invalid Number
     }
-
-    private static void PrintPrimes(int number)
-    {
-      int cont = 0;
-        for (int i = 2; i <= 13; i++){ 
-  for (int j = 1; j <= i; j++){   
- 
+    private static void PrintPrimes(int number){
+      if (number==0){
+        Console.WriteLine ("Invalid Number");
+      }
+      else if (number==1){
+        Console.WriteLine ("1 Is not Prime");
+      }
+      else{
+        int cont = 0;
+        for (int i = 2; i <= number; i++){  
+        for (int j = 1; j <= i; j++){
                if (i % j == 0){ 
                   cont = cont + 1;
                }
             }
-    
-    
             if (cont <= 2){ 
                Console.WriteLine(i);
-            }      
-            cont = 0;               
-    
-    
+            }
+            cont = 0;
          }
-  
          Console.ReadKey();
+      }
     }
   }
 }

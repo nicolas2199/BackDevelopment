870c05c7cbbbdc0d86fa57949bdd17c10e81d2c9
JramosF25 <91672832+JramosF25@users.noreply.github.com>
2022-02-22 22:18:01 -0500

    Update Program.cs

--- Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
+++ Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+//Lo edite desde el git porque no me estaba dejando subir los cambios desde mi PC :)
+using System;
 
 namespace PrintPrimes
 {
@@ -17,9 +18,41 @@ namespace PrintPrimes
 
     }
 
-    private static void PrintPrimes(int number)
+    private static void PrintPrimes(int numberLimit)
     {
-      throw new NotImplementedException();
+     int number=2, divisible=0;
+     while (number<=numberLimit)
+     {
+         if (numberLimit==0)
+         {
+             Console.WriteLine("Error: Invalid Number");   
+         }
+         else if (numberLimit==1)
+         {
+             Console.WriteLine("Error: 1 Is not Prime");    
+         }
+         else
+         {
+         for (int i = 1; i < number; i++)
+         {
+             if (number%i==0)
+             {
+                 divisible++;
+             }
+             if (divisible>2)
+             {
+                 break;
+             }
+         }
+         if (divisible==2)
+         {
+             Console.WriteLine("{0} es primo",number);    
+         }  
+         divisible = 0;
+         number++;
+      }
+      Console.ReadKey();
+     }
     }
   }
 }

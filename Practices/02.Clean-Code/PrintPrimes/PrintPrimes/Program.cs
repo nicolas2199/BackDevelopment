aa68bb7f8017c570ce6f70605c2cb7d531bc3d11
Valgon55 <valgon5500@gmail.com>
2022-02-23 18:52:01 -0500

    PrintPrimes

--- Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
+++ Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
@@ -2,48 +2,50 @@
 
 namespace PrintPrimes
 {
-  internal class Program
-  {
-    static void Main(string[] args)
-    {
-      PrintPrimes(13);
-      // 2,3,5,7,11,13
-      PrintPrimes(10);
-      // 2,3,5,7
-      PrintPrimes(0);
-      // Error: Invalid Number
-      PrintPrimes(1);
-      // Error: 1 Is not Prime
-
+internal class Program
+{
+static void Main(string[] args)
+{
+     
+  Console.WriteLine ("\nPrimes until 13");
+  PrintPrimes(13);
+  // 2,3,5,7,11,13
+     
+  Console.WriteLine ("\nPrimes until 10");
+  PrintPrimes(10);
+  // 2,3,5,7
+      
+  Console.WriteLine ("\nPrimes until 1");
+  PrintPrimes(1);
+  // Error: 1 Is not Prime
+      
+  Console.WriteLine ("\nPrimes until 0");
+  PrintPrimes(0);
+      
+  // Error: Invalid Number
     }
-
-    private static void PrintPrimes(int number)
-    {
-      //Console.WriteLine("ola");
-
-      int num, i, contador;
-      contador =0;
-
-      num = int.Parse(Console.ReadLine());
-
-      for (i=1; i<=num; i++ ){
-
-        if (num % i ==0){
-
-          contador= contador + 1;
-
-        }
-        if (contador > 2){
-          
-          Console.WriteLine("{0} No es primo", num);
-        }
-        else{
-           Console.WriteLine("{0} Es primo", num);
-        }
-        Console.ReadLine();
+  private static void PrintPrimes(int number){
+    if (number==0){
+        Console.WriteLine ("\nInvalid Number\n");
+      }
+      else if (number==1){
+        Console.WriteLine ("\n1 Is not Prime");
+      }
+      else{
+        int cont = 0;
+        for (int i = 2; i <= number; i++){  
+        for (int j = 1; j <= i; j++){
+          if (i % j == 0){ 
+            cont = cont + 1;
+               }
+            }
+            if (cont <= 2){ 
+               Console.WriteLine(i);
+            }
+            cont = 0;
+         }
+         Console.WriteLine();
+      }
     }
-
-
   }
-}
 }

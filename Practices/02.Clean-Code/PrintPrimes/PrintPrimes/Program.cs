b555daf61b46374ce224f4734d3bcfdd9e955eeb
nicolas2199 <nramirezv@correo.usbcali.edu.co>
2022-02-23 18:07:48 -0500

    Initial Version

--- Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
+++ Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
@@ -19,7 +19,41 @@ namespace PrintPrimes
 
     private static void PrintPrimes(int number)
     {
-      throw new NotImplementedException();
+      int n = 2;
+
+        
+      while(n <= number){
+
+            
+        bool esPrimo = true;
+
+            
+        for (int i = 2; i < n; i++) {
+            if(n % i == 0)
+            {
+              esPrimo = false;
+              break;
+           }
+
+          }
+
+            
+          if(esPrimo)
+          {
+           Console.WriteLine(n);
+          }
+            
+          n++;
+
+        }
+      switch (number){
+        case 0:
+          Console.WriteLine("Invalid Number");
+          break;
+        case 1:
+          Console.WriteLine("Is not prime");
+          break;
+      }    
     }
   }
 }

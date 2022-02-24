9f47ded0c0fb247e61a1f171f0b3694015af10bf
Luis0725 <luisym0725@gmail.com>
2022-02-23 21:12:39 -0500

    CleanVersion

---
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrintMultiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+//Write a program that prints the numbers from 1 to 100
+//But fot multiples of three print "M-3" instead of the number and for multiples of 5 print "M-5"
+//For numbers which are multiples of both 3 and 5 print "M-3-5" 

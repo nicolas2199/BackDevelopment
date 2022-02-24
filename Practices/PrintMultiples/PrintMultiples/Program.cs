ca01e36c5702f2052a8a66eeb2613047dde06dda
Danicubo <danycubo2002@hotmail.com>
2022-02-23 21:16:14 -0500

    CleanVersion

---
+++ Practices/PrintMultiples/PrintMultiples/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PrintMultiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintMultiples();
+            // 1, 2, M-3, 4, 5, M-3....
+        }
+
+    }
+}

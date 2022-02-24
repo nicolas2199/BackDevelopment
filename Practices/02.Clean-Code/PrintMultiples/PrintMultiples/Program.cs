f26207f250d57bb2886dd8adf5dfc6e9fcf4a3b7
heidy1523 <heidy1523diaz@gmail.com>
2022-02-23 21:32:31 -0500

    uuuu

--- Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -6,7 +6,20 @@ namespace PrintMultiples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           //imprimir numero
+           for(int CantidadEstablecida = 0 ; CantidadEstablecida<=100; CantidadEstablecida++){
+             //  Console.Write(CantidadEstablecida);
+             if (CantidadEstablecida%3==0 || CantidadEstablecida%5==0)
+             {
+                
+             }
+
+           }
+           //multiplos de 3
+
+           //multiplos de 5
+
+           //multiplos de los dos
         }
     }
 }

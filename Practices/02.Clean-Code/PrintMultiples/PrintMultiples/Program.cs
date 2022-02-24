1817f704bfe5cc84bf8b745e6dbeaf14b3b4ecff
Juan Felipe Rincon <Zothz@outlook.com>
2022-02-24 14:23:13 -0500

    Primera  Edicion

---
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace PrintMultiples
+{
+
+ internal class Program
+  {
+      private const int 
+static void Main(string[] args)
+    {
+    PrintFirstHundred();
+    }
+
+
+    private static void PrintFirstHundred(){
+        for (int currentNumber = 1; currentNumber <= 15; currentNumber++)
+        {
+            Console.WriteLine (CheckIfItsMultiple(currentNumber, 3));
+           /* if(currentNumber%5 == 0){
+            Console.WriteLine("M-5");
+            }
+            else
+            {
+            Console.WriteLine(currentNumber);
+            }*/
+        }
+    }
+
+    private static bool CheckIfItsMultiple(int currentNumber, int comparativeNumber){
+
+        return(currentNumber%comparativeNumber == 0);
+    }
+
+
+    
+}
+}

7c720c0d47b1359e6a3d7c8b608e850cda9c7a61
Nicol Henao <nicolandreahenaoossa@gmail.com>
2022-02-23 21:08:24 -0500

    Tarea Multiplos

---
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PrintMultiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void PrintMultiples(int number){
+            for(int i = 0; i < 100; i++){
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

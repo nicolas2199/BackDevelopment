333e1426c38b7b9f5ddf383c327350fdca648261
Deliox22 <juanfeosorio5a@gmail.com>
2022-02-25 15:25:05 -0500

    PrintMultiples

---
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PrintMultiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNumbers(1);
+        }
+
+        private static void PrintNumbers(int number)
+        {
+            for(int numberCounter = number; numberCounter <= 100; numberCounter++)
+            {
+                if(numberCounter%5==0){
+                 Console.Write("M-5, ");
+                }
+                else if(numberCounter%3==0){
+                 Console.Write("M-3, ");
+                }
+                else if(numberCounter%15==0){
+                 Console.Write("M-3-5, ");
+                }
+                else{
+                 Console.Write(numberCounter+", ");
+                } 
+            }
+        }
+    }    
+}

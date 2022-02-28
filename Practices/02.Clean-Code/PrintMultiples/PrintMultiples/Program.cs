c772f3cae0d248168e03f6f6948f47e94c51fdac
SeSaFx <sesafe2002@outlook.com>
2022-02-28 16:19:28 -0500

    Actualizacion

--- Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -39,17 +39,19 @@ namespace PrintMultiples
             for(int countOfnumbers=1 ; countOfnumbers<=maximumNumber; countOfnumbers++){
                 convertMultiples(countOfnumbers);
             }
-            
         }
 
         private static void convertMultiples(int countOfnumbers){
-            if (countOfnumbers %3 == 0 && countOfnumbers %5 == 0){ 
+            int multiplosDe3=0;
+            int multiplosDe5=0;
+
+            if (countOfnumbers %3 == multiplosDe3 && countOfnumbers %5 == multiplosDe5){ 
                 Console.WriteLine("M-3-5");
             } 
-            else if (countOfnumbers %5 == 0){ 
+            else if (countOfnumbers %5 == multiplosDe5){ 
                 Console.WriteLine("M-5");
             }  
-            else if (countOfnumbers %3 == 0){ 
+            else if (countOfnumbers %3 == multiplosDe3){ 
                 Console.WriteLine("M-3");
             }  
             else {

3b5915705d059c8038c2b8d7a685c2cd30474f10
JuanJoseEscobar <cheche.juan@hotmail.com>
2022-03-01 12:50:22 -0500

    se realizo el ejercicio de multiples

--- Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -11,10 +11,68 @@ namespace PrintMultiples
 
         private static void PrintMultiples(int number)
         {
-            for (int i = 1; i < number; i++)
-            {
-                Console.WriteLine(i);
-            }
+            Console.Write(getNumbersUntil(number));
         }
+
+        private static string getNumbersUntil(int number)
+        {
+            string ListNumbers = "";
+            for (int i = 1; i <= number; i++)
+            {
+                //ListNumbers += isMultiple(i , 3) ? isMultiple(i , 5) ? (i - 3 - 5)+"" : (i - 3)+"" : isMultiple(i , 5) ? (i-5)+"" : (i)+"";
+
+                if(isMultiple(i , 3)){
+
+                    if(isMultiple(i , 5))
+                    {
+                        ListNumbers += (i-3-5)+"";
+                    }else{
+                        ListNumbers += (i-3)+"";
+                    }
+
+                }else if(isMultiple(i , 5)){
+
+                    ListNumbers += (i-5)+"";
+
+                }else{
+
+                    ListNumbers += (i)+"";
+
+                }
+
+                ListNumbers += i == number ? ".": ",";
+            }
+
+            return ListNumbers;
+        }
+
+        private static bool isMultiple(int number, int multiple)
+        {
+            return (number % multiple == 0);
+        }
+
+        /************** version 0.1 ************
+        /* private static void PrintMultiples(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                if(i % 3 == 0){
+                    if(i % 5 == 0){
+                        Console.Write($"c3:{i-3-5},");
+                    }
+                    else
+                    {
+                        Console.Write($"c1:{i-3},");
+                    }
+                }
+                else if(i % 5 == 0){
+                    Console.Write($"c2:{i-5},");
+                }
+                else{
+                    Console.Write($"c0:{i},");
+                }
+            }
+        } */
+        /************** version 0.1 ************/
     }
 }

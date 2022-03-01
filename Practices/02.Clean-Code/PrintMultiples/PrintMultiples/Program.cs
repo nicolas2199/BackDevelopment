5a02b019662666816e8f8f91a5cc67f946ccaacd
Nicol Henao <nicolandreahenaoossa@gmail.com>
2022-03-01 16:30:12 -0500

    Tarea Multiplos

--- Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -1,38 +1,39 @@
 ﻿using System;
 
-namespace PrintMultiples
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
+namespace PrintMultiples {
+    public class Program {
+        static void Main(string[] args) {
             PrintMultiples();
-
-            
         }
 
-        private static void PrintMultiples()
-        {
-            bool isMultiple;
-
-            for(int i = 1; i <= 100; i++)
-            {
-                isMultiple = false;
-
-                if(i%3 == 0)
-                {
-                    isMultiple = true;
+        private static void PrintMultiples() {
+            for (int i = 1; i <= 100; i++) {
+                if (FindMultiple(i) == 3) {
+                    Console.Write("M-3, ");
                 }
-
-                if(isMultiple == true)
-                {
-                    Console.WriteLine("M-3");
-                    isMultiple = true;
-                } else {
-                    Console.WriteLine(i);
-                    isMultiple = false;
+                if (FindMultiple(i) == 5) {
+                    Console.Write("M-5, ");
+                }
+                if (FindMultiple(i) == 1) {
+                    Console.Write("M-3-5, ");
+                }
+                if (FindMultiple(i) == 0) {
+                    Console.Write($"{i}, ");
                 }
             }
+        }
+
+        public static int FindMultiple(int number) {
+            if (number % 3 == 0) {
+                if (number % 5 == 0) {
+                    return 1;
+                }
+                return 3;
+            }
+            if (number % 5 == 0) {
+                return 5;
+            }
+            return 0;
         }
     }
 }

78168fc5e2e1e93e82171e8ffcf72542f2a0ebc7
Valgon55 <valgon5500@gmail.com>
2022-03-02 17:45:51 -0500

    PrintMultiples

---
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace PrintMultiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Números del 1 al 100: ");
+            PrintMultiples(100);
+
+        }
+        
+        static bool CheckMultiples (int number, int multiple){
+
+            bool TestMultiples = false;
+
+            if (number %multiple ==0){
+                TestMultiples = true;
+            }
+            else{
+                TestMultiples = false;
+            }
+
+            return TestMultiples;
+        }
+    
+        static void PrintMultiples (int TargetNumber ){
+            int FirstNumber = 1;
+        
+            for (int NumberCounter = FirstNumber; NumberCounter <= TargetNumber; NumberCounter ++){
+
+                if ((CheckMultiples(NumberCounter,5)==true)&(CheckMultiples(NumberCounter,3))){
+                    Console.WriteLine ("M-3-5 ");
+                }
+                else if (CheckMultiples(NumberCounter,5)==true){
+                    Console.WriteLine ("M-5 ");
+                }
+
+                else if (CheckMultiples(NumberCounter,3)==true){
+                    Console.WriteLine ("M-3 ");
+                }
+                else{
+                    Console.WriteLine (NumberCounter);
+                }
+            }
+        }
+    }
+}
+
+    
+

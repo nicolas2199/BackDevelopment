8d474fcbec7b3c4f18d0c1896f9116e963e1fb79
Nicol Henao <nicolandreahenaoossa@gmail.com>
2022-03-02 17:48:54 -0500

    cooo

--- Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -4,22 +4,55 @@ namespace PrintMultiples
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-           //imprimir numero
-           for(int CantidadEstablecida = 0 ; CantidadEstablecida<=100; CantidadEstablecida++){
-             //  Console.Write(CantidadEstablecida);
-             if (CantidadEstablecida%3==0 || CantidadEstablecida%5==0)
-             {
-                
-             }
-
-           }
-           //multiplos de 3
-
-           //multiplos de 5
-
-           //multiplos de los dos
+        static void Main(string[] args){
+          PrintMultiples();
         }
-    }
-}
+        
+        static void PrintMultiples(){
+           string texto1="m-3";
+           string texto2="m-5";
+           string texto3="m-1";
+           int variable=0;
+               Console.WriteLine("prueba");
+
+
+           for( int i = 0 ; i<=100; i++){
+          variable=NumerosALetras(i);
+             if (variable==3){
+               
+               Console.WriteLine(texto1);
+          
+             }
+            else if(variable==5){
+               Console.WriteLine(texto2);
+             }
+            else  if(variable==1){
+               Console.WriteLine(texto3);
+             }
+             Console.WriteLine("holi");
+
+        }
+        }
+
+        public static int NumerosALetras(int value){
+
+          if (value %3==0 )
+          {
+            return 5;
+             }
+           
+           if(value %5==0){
+           return 1;
+
+          }
+         
+          //else if(value%3==0&&value%5==0){
+            //return numeroATexto=Console.writelines( "m-3m-5");
+         // }
+          return 0;
+
+          }
+     
+        }
+
+        }

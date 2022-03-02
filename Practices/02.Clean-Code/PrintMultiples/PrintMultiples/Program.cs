a19e02016f0f10e5d66a7932e1b4584b9aadeee3
heidy1523 <heidy1523diaz@gmail.com>
2022-03-02 18:38:47 -0500

    ghg

--- Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -11,9 +11,9 @@ namespace PrintMultiples
         static void PrintMultiples(){
            string texto1="m-3";
            string texto2="m-5";
-           string texto3="m-1";
+           string texto3="m-3m-5";
            int variable=0;
-               Console.WriteLine("prueba");
+              
 
 
            for( int i = 0 ; i<=100; i++){
@@ -29,26 +29,28 @@ namespace PrintMultiples
             else  if(variable==1){
                Console.WriteLine(texto3);
              }
-             Console.WriteLine("holi");
+             else 
+             Console.WriteLine(i);
 
         }
+
         }
 
         public static int NumerosALetras(int value){
 
           if (value %3==0 )
           {
-            return 5;
+            return 3;
              }
            
            if(value %5==0){
-           return 1;
+           return 5;
 
           }
          
-          //else if(value%3==0&&value%5==0){
-            //return numeroATexto=Console.writelines( "m-3m-5");
-         // }
+          else if(value%3==0 && value%5==0){
+          return 1 ;
+          }
           return 0;
 
           }

11582b4ca9ba2f41fdb494ebe8fa22c50c9efc4b
SeSaFx <sesafe2002@outlook.com>
2022-03-02 21:21:12 -0500

    Actualizacion

---
+++ Practices/02.Clean-Code/Debugging/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            string  sProdName = "widget";
+            int iUnitQty = 100;
+            double dUnitCost = 1.03;
+
+            Debug.WriteLine("Debug: iniciamos depuracion del producto");
+            
+        }
+    }
+}

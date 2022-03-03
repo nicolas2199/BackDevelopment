e492ec4fb26f002175771cd57d7684a446c2327f
JramosF25 <juanjorf25@gmail.com>
2022-03-02 20:26:15 -0500

    Creacion Debug

---
+++ Practices/02.Clean-Code/Debugging/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string sProdName = "Widget";
+            int iUnitQty = 100;
+            double dUnitCost = 1.03;
+            Debug.WriteLine("Debug: Iniciando depuracion de producto");
+        }
+    }
+}

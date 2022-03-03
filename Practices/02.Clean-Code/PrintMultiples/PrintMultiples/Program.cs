c140f4b00dac95ca8f2292b208279fce13925c69
Pifex <89953641+Pifex@users.noreply.github.com>
2022-03-02 19:48:07 -0500

    Alo?

---
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+    using system; 
+
+
+namespace PrimtMultiples
+{
+    internal class Name
+    {
+        static void Main(string[] args)
+        {
+            countNumbers();
+        }
+
+        private static void countNumbers()
+        {
+            for (int currentNumber = 1; currentNumber <= 100; currentNumber++)
+            {
+                if (isMultipleOf(currentNumber, 3) || isMultipleOf(currentNumber, 5) || 
+                (isMultipleOf(currentNumber, 3) && isMultipleOf(currentNumber, 5)))
+                {   
+                    if (isMultipleOf(currentNumber, 3) && isMultipleOf(currentNumber, 5))
+                    {
+                        Console.Write("M-3-5");   
+                    }else
+                    {
+                        if (isMultipleOf(currentNumber, 3))
+                        {
+                            Console.Write("M-3");   
+                        }
+                        if (isMultipleOf(currentNumber, 5))
+                        {
+                            Console.Write("M-5");   
+                        }
+                    }
+                }
+                else
+                {
+                    Console.Write(currentNumber);
+                }
+                if (currentNumber<100)
+                {   
+                    Console.Write(", ");
+                }
+            }
+
+        }
+        // encontrar multiplos de 
+        private static bool isMultipleOf(int currentNumber, int baseNumber)
+        {
+            return currentNumber % baseNumber == 0;
+        }
+
+    }
+
+}

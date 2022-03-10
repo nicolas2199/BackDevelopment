fdb3cbba5a2a738a8d8f8c6386c8dff4fc69940b
heidy1523 <heidy1523diaz@gmail.com>
2022-03-10 13:12:53 -0500

    q

--- Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -4,55 +4,77 @@ namespace PrintMultiples
 {
     class Program
     {
-        static void Main(string[] args){
-          PrintMultiples();
-        }
-        
-        static void PrintMultiples(){
-           string mensajeMultiplo3="m-3";
-           string mensajeMultiplo5="m-5";
-           string mensajeMultiploAmbos="m-3m-5";
-           int variable=0;
-              
-           for( int llenarNumeros = 0 ; llenarNumeros<=100; llenarNumeros++){
-          variable=NumerosALetras(llenarNumeros);
-             if (variable==3){
-               
-               Console.WriteLine(mensajeMultiplo3);
-          
-             }
-            else if(variable==5){
-               Console.WriteLine(mensajeMultiplo5);
-             }
-            else  if(variable==1){
-               Console.WriteLine(mensajeMultiploAmbos);
-             }
-             else 
-             Console.WriteLine(llenarNumeros);
-
+        static void Main(string[] args)
+        {
+            PrintMultiples();
         }
 
+        static void PrintMultiples()
+        {
+            string mensajeMultiplo3 = "m-3";
+            string mensajeMultiplo5 = "m-5";
+            string mensajeMultiploAmbos = "m-3m-5";
+            int variable = 0;
+
+            for (int llenarNumeros = 0; llenarNumeros <= 100; llenarNumeros++)
+            {
+                variable = NumerosALetras(llenarNumeros);
+                if (variable == 3)
+                {
+
+                    Console.WriteLine(mensajeMultiplo3);
+
+                }
+                else if (variable == 5)
+                {
+                    Console.WriteLine(mensajeMultiplo5);
+                }
+                else if (variable == 1)
+                {
+                    Console.WriteLine(mensajeMultiploAmbos);
+                }
+                else
+                    Console.WriteLine(llenarNumeros);
+
+            }
+
+        }
+        private static int multiploDeTres(int numero)
+        {
+            int resultado = numero % 3;
+            return resultado;
+
+        }
+        private static int multiploDeCinco(int numero)
+        {
+            int resultado = numero % 5;
+            return resultado;
+
         }
 
-        public static int NumerosALetras(int value){
 
-          if (value %3==0 )
-          {
-            return 3;
-             }
-           
-           if(value %5==0){
-           return 5;
+        public static int NumerosALetras(int numero)
+        {
+            if (multiploDeTres(numero) == 0 && multiploDeCinco(numero) == 0)
+            {
+                return 1;
+            }
 
-          }
-         
-          else if(value%3==0 && value%5==0){
-          return 1 ;
-          }
-          return 0;
+            else if (multiploDeTres(numero) == 0)
+            {
+                return 3;
+            }
 
-          }
-     
+            else if (multiploDeCinco(numero) == 0)
+            {
+                return 5;
+
+            }
+
+            return 0;
+
         }
 
-        }
+    }
+
+}

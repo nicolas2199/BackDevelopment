b423308889b7512ded2b066d13d714197a213e79
Deliox22 <juanfeosorio5a@gmail.com>
2022-03-09 21:23:55 -0500

    exam

--- Exams/Exam-01/CompressString/Program.cs
+++ Exams/Exam-01/CompressString/Program.cs
@@ -1,14 +1,4 @@
-﻿/// <summary>
-/// Implemente un método para realizar la compresión básica de cadenas utilizando el recuento de caracteres repetidos.
-/// Criterios de Aceptación:
-/// - la cadena aabcccccaaa se convertiría en a2b1c5a3.
-/// - Si la longitud de la cadena "comprimida" es mayor la longitud de la cadena original, su método debería devolver la cadena original.
-/// - La cadena no puede ser nula o vacia
-/// - La longitud de la cadena debe ser menor o igual a 255 caracteres
-/// - La cadena solo debe permitir caracteres alfabeticos ([A-Z,a-z])
-/// - Los caracteres se diferencian por mayusculas o minusculas (Case Sensitive)
-/// </summary>
-
+﻿
 using System;
 
 namespace CompressString
@@ -34,15 +24,15 @@ namespace CompressString
       }
      static void Compress(string input)
        {
-        int MAX_LENGTH= 254;
+        int MAX_LENGTH= 255;
 
-          if (input == "")
+        if (string.IsNullOrEmpty(input))
             {
              Console.WriteLine("Error: the string must not be null or empty.");
              return;
             }  
 
-        if (input.Length > MAX_LENGTH)
+        else if (input.Length > MAX_LENGTH)
             {
              Console.WriteLine("Error: The length of the string must be less than 255 characters.");
             }
@@ -79,7 +69,6 @@ namespace CompressString
                 }
                 else
                 {
-                    // Cuando el carácter repetido cambia, el carácter se insertará
                     ZipResult += index;
                     ZipResult += number.ToString();
                     if(i + 1 < chainLength)
@@ -93,8 +82,6 @@ namespace CompressString
                     number = 1;
                 }
             }
-    
-                    // Necesita actualizar la cadena al final de la función, porque el último conjunto de caracteres repetidos no se ha colocado en la cadena comprimida
             ZipResult += index;
             ZipResult += number.ToString();
             

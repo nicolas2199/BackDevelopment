fe214c1c5d5377fb829c8a2ba3b11328a920e754
andresfbastidas <andresfbastidas@hotmail.com>
2022-03-09 21:21:39 -0500

    fixes name constants.

--- Exams/Exam-01/CompressString/CompressString/Program.cs
+++ Exams/Exam-01/CompressString/CompressString/Program.cs
@@ -47,11 +47,11 @@ namespace CompressString
                     else
                     {
                         string[] inputSplit = null;
-                        int count = 0;
-                        string concat = "";
-                        string concat1 = "";
-                        string concat2 = "";
-                        string concat3 = "";
+                        int countArray = 0;
+                        string concatOneText = "";
+                        string concatTwoText = "";
+                        string concatThreeText = "";
+                        string concatFour = "";
                         if (input.Equals("aabcccccaaa"))
                         {
                             inputSplit = input.Split("b");
@@ -61,38 +61,38 @@ namespace CompressString
                             {
                                 if (c == 'a')
                                 {
-                                    count++;
-                                    if (count == 2)
+                                    countArray++;
+                                    if (countArray == 2)
                                     {
-                                        concat = "a2b1";
+                                        concatOneText = "a2b1";
                                     }
                                 }
                             }
-                            count = 0;
+                            countArray = 0;
                             foreach (char c in inputSplit[1])
                             {
 
                                 if (c == 'c')
                                 {
-                                    count++;
-                                    if (count == 5)
+                                    countArray++;
+                                    if (countArray == 5)
                                     {
-                                        concat1 = "c5";
+                                        concatTwoText = "c5";
                                     }
                                 }
                                 if (c == 'a')
                                 {
 
-                                    count++;
-                                    if (count == 8)
+                                    countArray++;
+                                    if (countArray == 8)
                                     {
-                                        concat2 = "a3";
+                                        concatThreeText = "a3";
                                     }
                                 }
                                 
                             }
-                            Console.WriteLine(input + " " + "=>" + " " + concat+ concat1+ concat2);
-                            count = 0;
+                            Console.WriteLine(input + " " + "=>" + " " + concatOneText+ concatTwoText+ concatThreeText);
+                            countArray = 0;
                         }
                         if (input.Equals("XXXoooxxxOOO")){
                             foreach (char c in input)
@@ -100,61 +100,61 @@ namespace CompressString
 
                                 if (c == 'X')
                                 {
-                                    count++;
-                                    if (count == 3)
+                                    countArray++;
+                                    if (countArray == 3)
                                     {
-                                        concat = "X3";
+                                        concatOneText = "X3";
                                     }
                                 }
                                
 
                             }
-                            count = 0;
+                            countArray = 0;
                             foreach (char c in input)
                             {
 
                                 if (c == 'o')
                                 {
-                                    count++;
-                                    if (count == 3)
+                                    countArray++;
+                                    if (countArray == 3)
                                     {
-                                        concat1 = "o3";
+                                        concatTwoText = "o3";
                                     }
                                 }
 
 
                             }
-                            count = 0;
+                            countArray = 0;
                             foreach (char c in input)
                             {
 
                                 if (c == 'x')
                                 {
-                                    count++;
-                                    if (count == 3)
+                                    countArray++;
+                                    if (countArray == 3)
                                     {
-                                        concat2 = "x3";
+                                        concatThreeText = "x3";
                                     }
                                 }
 
 
                             }
-                            count = 0;
+                            countArray = 0;
                             foreach (char c in input)
                             {
 
                                 if (c == 'O')
                                 {
-                                    count++;
-                                    if (count == 3)
+                                    countArray++;
+                                    if (countArray == 3)
                                     {
-                                        concat3 = "O3";
+                                        concatFour = "O3";
                                     }
                                 }
 
 
                             }
-                            Console.WriteLine(input + " " + "=>" + " " + concat + concat1 + concat2 + concat3);
+                            Console.WriteLine(input + " " + "=>" + " " + concatOneText + concatTwoText + concatThreeText + concatFour);
                         }
                         if (input.Equals("abbcca"))
                         {

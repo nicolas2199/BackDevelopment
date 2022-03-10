0fe73e5aafa367aee75fdc7bceabbd9e8fd4dafa
Juan Felipe Rincon <Zothz@outlook.com>
2022-03-10 01:09:57 -0500

    Entrega

--- Practices/02.Clean-Code/Debugging/Program.cs
+++ Practices/02.Clean-Code/Debugging/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+
+
+
+namespace Debugging
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            string sProdName = "Widget";
+            int iUnitQty = 100;
+            double dUnitCost = 1.03;
+
+                Debug.Indent();
+                Debug.WriteLine("Debug: Iniciando depuracion de producto");
+
+                Debug.WriteLine("El Nombre del producto es: " + sProdName);
+                Debug.WriteLine("El numero de unidades es: " + iUnitQty.ToString());
+                Debug.WriteLine("El valor por unidad es: " + dUnitCost.ToString());
+
+                System.Xml.XmlDocument oxml = new System.Xml.XmlDocument();
+                Debug.WriteLine(oxml);
+        }
+    }
+
+}

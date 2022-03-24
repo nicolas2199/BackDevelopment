cc444d6617ecd0e8c382371678303a2915ca1a93
David Anduquia <daledro-1998@hotmail.com>
2022-03-23 19:08:39 -0500

    traje la informacion del ultimo commit de la rama no oficial de la clase davancep y original/davancep ramas para borrar

--- Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
+++ Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
@@ -2,24 +2,40 @@
 
 namespace PrintPrimes
 {
-  internal class Program
-  {
-    static void Main(string[] args)
+    internal class Program
     {
-      PrintPrimes(13);
-      // 2,3,5,7,11,13
-      PrintPrimes(10);
-      // 2,3,5,7
-      PrintPrimes(0);
-      // Error: Invalid Number
-      PrintPrimes(1);
-      // Error: 1 Is not Prime
+        static void Main(string[] args)
+        {
+            PrintPrimes(13);
+            // 2,3,5,7,11,13
+            PrintPrimes(10);
+            // 2,3,5,7
+            PrintPrimes(0);
+            // Error: Invalid Number
+            PrintPrimes(1);
+            // Error: 1 Is not Prime
 
-    }
+        }
 
-    private static void PrintPrimes(int number)
-    {
-      throw new NotImplementedException();
+        private static void PrintPrimes(int number)
+        {
+            for (int i = 0; i <= number; i++)
+            {
+                if (number >= 0 && number <= 1)
+                {
+                    Console.Write("Numero invalido");
+
+                }
+                else
+                {
+
+                    if (i % 2 != 0)
+                    {
+                        //Console.Write("No es primo");
+                        Console.Write(i + "   ");
+                    }
+                }
+            }
+        }
     }
-  }
 }

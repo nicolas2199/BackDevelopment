c4ae47d905f687320c9d91c815f37652f3fe9714
Julio Robles <jucer74@hotmail.com>
2022-03-23 20:10:27 -0500

    Update

--- Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -1,22 +1,36 @@
-﻿/// <summary>
-/// Objetivo:
-///   Imprima los números del 1 a 100, separados por coma.
-///   - Pero para múltiplos de tres imprima “M-3” en lugar del número
-///   - Para los múltiplos de cinco imprima “M-5”
-///   - Para números que son múltiplos de tres y cinco, escriba “M-3-5”
-///   - Para los demas imprima solo el numero.
-///  Ejemplo:
-///   1,2,M-3,4,M-5,M-3,7,8,M-3,M-5,11,M-3,13,14,M-3-5,......
-/// </summary>
-using System;
+﻿using System;
 
 namespace PrintMultiples
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            /* numeros del 1 al 100 */ 
+            /* cada multiplo de 3  se escribe m-3 */
+            /* cada multiplo de 5 se escribe M-5 */
+           /* si es multiplo de ambos se escribe m-3-5 */
+            Console.WriteLine("Del 1 al 100");
+            printmultiples();
+        }
+
+        private static void printmultiples(){
+            
+            for (int increase_Number=1;increase_Number<=100;increase_Number++){
+               
+                if(increase_Number%15==0){
+                    Console.Write(increase_Number+" M-3-5, ");
+                }
+                else if(increase_Number%5==0){
+                     Console.Write(increase_Number+" M-5, ");
+                }
+                else if(increase_Number%3==0){
+                    Console.Write(increase_Number+" M-3, ");
+                }
+                else{
+                     Console.Write(increase_Number+", ");
+                }
+            }
         }
     }
 }

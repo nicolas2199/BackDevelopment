25c10240d9d7b03a9e49fc32a3deede5a8b4e96e
nicolas2199 <nramirezv@correo.usbcali.edu.co>
2022-04-27 16:58:43 -0500

    Parcial 2

---
+++ Practices/01.OOP/Sample01/Person.cs
@@ -0,0 +1,21 @@
+
+
+public class Person
+{
+
+private string Name;
+private string LastName;
+private string Gender;
+private int Age;
+private float Tall;
+private float PhoneNumber; 
+
+
+public string InsertName
+{
+   
+}
+
+}
+
+    
---
+++ Practices/01.OOP/Sample03/Person.cs
@@ -0,0 +1,41 @@
+public class Person
+{
+    //Attributes
+    //private int id;
+    //private string firstName;
+    //private string lastName;
+    //private DateTime dateOfbirth;
+    //private char sex;
+    //Properties
+    //public int id
+   // {
+       // get
+        //{
+           // return this.id;
+        //}
+        //set
+        //{
+          //  this.id = value;
+        //}
+    //}
+
+    public int Id{get;set;}
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public char Sex { get; set; }
+
+    public Person()
+    {
+
+    } 
+
+    public Person(int id, string firstName,string lastName, dateTime dateOfBirth,char sex)
+    {
+         this.Id=Id;
+         this.FirstName = firstName;
+         this.LastName =lastName;
+         this.DateOfBirth = dateOfBirth;
+         this.Sex = sex;
+    }
+}
